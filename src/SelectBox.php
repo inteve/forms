c38c2973a8dba5f925d4188431f969ee7757d26b
Jan Pecha <janpecha@email.cz>
2021-10-08 08:35:45 +0200

    Added SelectBox

---
+++ src/SelectBox.php
@@ -0,0 +1,57 @@
+<?php
+
+	namespace Inteve\Forms;
+
+	use Nette;
+
+
+	class SelectBox extends Nette\Forms\Controls\SelectBox
+	{
+		/** @var callable|NULL */
+		private $itemsProvider;
+
+
+		/**
+		 * @param string|NULL $label
+		 * @param array<mixed>|null $items
+		 */
+		public function __construct($label = NULL, array $items = NULL)
+		{
+			parent::__construct($label, $items);
+			$this->control->appendAttribute('class', 'input--selectbox');
+		}
+
+
+		/**
+		 * @param  string $url
+		 * @return static
+		 */
+		public function setRemoteSource($url, callable $itemsProvider)
+		{
+			$this->setHtmlAttribute('data-source-url', $url);
+			$this->itemsProvider = $itemsProvider;
+			return $this;
+		}
+
+
+		/**
+		 * @param  string $url
+		 * @return static
+		 */
+		public function enableCreate($url)
+		{
+			$this->setHtmlAttribute('data-create-url', $url);
+			return $this;
+		}
+
+
+		public function loadHttpData()
+		{
+			parent::loadHttpData();
+
+			if ($this->itemsProvider !== NULL && ((string) $this->value) !== '') {
+				$items = call_user_func($this->itemsProvider, $this->value);
+				$this->setItems($items);
+			}
+		}
+	}
